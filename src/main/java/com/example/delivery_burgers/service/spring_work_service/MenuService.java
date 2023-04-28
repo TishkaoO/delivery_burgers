d930a36068f8a440665ffdd12f00b9af3a17c003
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-04-28 19:32:15 +0300

    переписал методы сервисов через stream

--- src/main/java/com/example/delivery_burgers/service/spring_work_service/MenuService.java
+++ src/main/java/com/example/delivery_burgers/service/spring_work_service/MenuService.java
@@ -6,10 +6,8 @@ import com.example.delivery_burgers.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
