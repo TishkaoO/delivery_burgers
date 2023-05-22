0e2442853a91a77a2671613f34565391a921b978
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-05-22 08:38:12 +0300

    добавил правильные дто

---
+++ src/main/java/com/example/delivery_burgers/api/dto/AscDto.java
@@ -0,0 +1,15 @@
+package com.example.delivery_burgers.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AscDto {
+
+    private String answer;
+}
---
+++ src/main/java/com/example/delivery_burgers/api/dto/BurgerDto.java
@@ -0,0 +1,30 @@
+package com.example.delivery_burgers.api.dto;
+
+import com.example.delivery_burgers.api.validation.Operation;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class BurgerDto {
+
+    @NotBlank(message = "Name must be not empty")
+    @Length(max = 50, message = "Name length must be smaller than 50", groups = {Operation.OnUpdate.class,
+            Operation.OnCreate.class})
+    private String name;
+
+    @NotBlank(message = "Description must be not empty")
+    @Length(max = 255, message = "Description length must be smaller than 255", groups = {Operation.OnUpdate.class,
+            Operation.OnDelete.class})
+    private String description;
+
+    @Min(value = 1, message = "Price must be more than 0", groups = {Operation.OnUpdate.class,
+            Operation.OnCreate.class})
+    private double price;
+
+    @JsonProperty(namespace = "is_spicy")
+    private boolean isSpicy;
+}
