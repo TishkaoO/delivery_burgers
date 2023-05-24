84993feb68cebe1322cc88a3d66eb208587c4b55
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-05-24 13:10:12 +0300

    настроена миграция для liquibase

--- src/main/resources/db/scripts/004_ddl_create_burger_table.sql
+++ src/main/resources/db/scripts/004_ddl_create_burger_table.sql
@@ -3,5 +3,6 @@ id SERIAL PRIMARY KEY,
 name VARCHAR not null unique,
 description VARCHAR not null,
 price VARCHAR not null,
+is_spicy VARCHAR not null,
 dish_id int references orders(id)
 );
--- src/main/resources/db/scripts/006_dml_insert_burger.sql
+++ src/main/resources/db/scripts/006_dml_insert_burger.sql
@@ -1,11 +1,11 @@
 INSERT INTO burger (name, description, price, is_spicy)
-VALUES ('бургер1', 'Описание блюда', 9.99, false);
+VALUES ('бургер1', 'Описание блюда1', 9.99, false);
 
 INSERT INTO burger (name, description, price, is_spicy)
-VALUES ('бургер2', 'Описание блюда', 4.99, true);
+VALUES ('бургер2', 'Описание блюда2', 4.99, true);
 
 INSERT INTO burger (name, description, price, is_spicy)
-VALUES ('бургер3', 'Описание блюда', 3.99, false);
+VALUES ('бургер3', 'Описание блюда3', 3.99, false);
 
 INSERT INTO burger (name, description, price, is_spicy)
-VALUES ('бургер4', 'Описание блюда', 2.99, true);
+VALUES ('бургер4', 'Описание блюда4', 2.99, true);
